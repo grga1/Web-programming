8ea7d0caef41ef28de58d5804e533328f771af27
Filip Iliev <151552696+grga1@users.noreply.github.com>
2026-02-15 16:42:08 +0100

    Add files via upload

---
+++ kolokvium2025-grupa1/kol2025-g1/src/main/java/mk/ukim/finki/wp/kol2025g3/repository/ExpenseRepository.java
@@ -0,0 +1,6 @@
+package mk.ukim.finki.wp.kol2025g3.repository;
+
+import mk.ukim.finki.wp.kol2025g3.model.Expense;
+
+public interface ExpenseRepository extends  JpaSpecificationRepository<Expense,Long> {
+}
---
+++ kolokvium2025-grupa1/kol2025-g1/src/main/java/mk/ukim/finki/wp/kol2025g3/repository/VendorRepository.java
@@ -0,0 +1,6 @@
+package mk.ukim.finki.wp.kol2025g3.repository;
+
+import mk.ukim.finki.wp.kol2025g3.model.Vendor;
+
+public interface VendorRepository extends JpaSpecificationRepository<Vendor,Long> {
+}
